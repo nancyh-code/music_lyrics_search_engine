c1618a5d40f08050b4dee4ca05f5ec3b357fcc54
Nancy Hernandez <hernandezyanezhotmail.com>
2022-03-30 18:23:10 -0300

    Adding LyricApp

---
+++ src/components/LyricApp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./Form";
+
+const LyricApp = () => {
+  return (
+    <>
+      <header>Music lyrics search engine</header>
+      <Form />
+      <main></main>
+    </>
+  );
+};
+
+export default LyricApp;
