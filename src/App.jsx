1757c2ac4627d0a2f8b5186aae10d5fef0c85d03
Nancy Hernandez <hernandezyanezhotmail.com>
2022-03-30 18:21:57 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import LyricApp from "./components/LyricApp";
+
+function App() {
+  return (
+    <div>
+      <LyricApp />
+    </div>
+  );
+}
+
+export default App;
